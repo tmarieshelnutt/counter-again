08aeba209ae215176753a87988789e9f75edf60d
Püki <52825615+tmarieshelnutt@users.noreply.github.com>
2023-05-25 12:13:40 +0000

    round three

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,39 @@
+//import react into the bundle
 import React from "react";
+import ReactDOM from "react-dom";
+// import Card from './component/Card.jsx'
+import PropTypes from 'prop-types';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// include your styles into the webpack bundle
+// import "../styles/index.css";
+//import "./component/src/icons";
+//import your own components
+function SimpleCounter(props){
+    return (<div className="clockCounter" >
+        <div className="clock"><i class="far fa-clock"></i></div>
+        <div className="four">{props.digitFour % 10}</div>
+        <div className="three">{props.digitThree % 10}</div>
+        <div className="two">{props.digitTwo % 10}</div>
+        <div className="one">{props.digitOne % 10}</div>
+    </div>);
+}
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+SimpleCounter.proptypes = {
+    digitFour: PropTypes.number,
+    digitThree: PropTypes.number,
+    digitTwo: PropTypes.number,
+    digitOne: PropTypes.number
 };
+let counter = 0;
+setInterval(function(){
+    const four = Math.floor(counter/1000);
+    const three = Math.floor(counter/100);
+    const two = Math.floor(counter/10);
+    const one = Math.floor(counter/1);
+    counter++;
+  ReactDOM.render(
+    <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />, document.querySelector("#app")
+    );  
+},1000);
 
 export default Home;
